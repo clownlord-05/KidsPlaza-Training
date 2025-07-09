e63e8980a41d53acb28c53ddde0ac346d6867d61
clownlord-05 <tungadamnmt@gmail.com>
2025-07-09 22:02:57 +0700

    demo girdcontrol trong DX connect sql sever

---
+++ 2025-07-09/Dx-sql/Dx-sql/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace Dx_sql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtis = new System.Windows.Forms.TextBox();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.usersBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.qlnguoidungDataSet1 = new Dx_sql.qlnguoidungDataSet1();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colID = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colUsername = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colPassword = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colBirthDate = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colIsActive = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.qlnguoidungDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.qlnguoidungDataSet = new Dx_sql.qlnguoidungDataSet();
+            this.gridLookUpEdit1 = new DevExpress.XtraEditors.GridLookUpEdit();
+            this.gridLookUpEdit1View = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.usersTableAdapter = new Dx_sql.qlnguoidungDataSet1TableAdapters.UsersTableAdapter();
+            this.txtdate = new System.Windows.Forms.TextBox();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qlnguoidungDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qlnguoidungDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qlnguoidungDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLookUpEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLookUpEdit1View)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnthem
+            // 
+            this.btnthem.Location = new System.Drawing.Point(617, 28);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(104, 52);
+            this.btnthem.TabIndex = 0;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(617, 100);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(104, 52);
+            this.btnsua.TabIndex = 1;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(617, 175);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(104, 52);
+            this.btnxoa.TabIndex = 2;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(53, 28);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(12, 16);
+            this.labelControl1.TabIndex = 4;
+            this.labelControl1.Text = "ID";
+            this.labelControl1.Click += new System.EventHandler(this.labelControl1_Click);
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(53, 89);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(63, 16);
+            this.labelControl2.TabIndex = 5;
+            this.labelControl2.Text = "User Name";
+            this.labelControl2.Click += new System.EventHandler(this.labelControl2_Click);
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(52, 163);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(64, 16);
+            this.labelControl3.TabIndex = 6;
+            this.labelControl3.Text = "Pass World";
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(52, 228);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(44, 16);
+            this.labelControl4.TabIndex = 7;
+            this.labelControl4.Text = "IsActive";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(211, 43);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(364, 22);
+            this.txtid.TabIndex = 8;
+            this.txtid.TextChanged += new System.EventHandler(this.txtid_TextChanged);
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(211, 86);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(364, 22);
+            this.txtname.TabIndex = 9;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(211, 175);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(364, 22);
+            this.txtpass.TabIndex = 10;
+            // 
+            // txtis
+            // 
+            this.txtis.Location = new System.Drawing.Point(211, 222);
+            this.txtis.Name = "txtis";
+            this.txtis.Size = new System.Drawing.Size(364, 22);
+            this.txtis.TabIndex = 11;
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.usersBindingSource;
+            this.gridControl1.Location = new System.Drawing.Point(52, 250);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(669, 190);
+            this.gridControl1.TabIndex = 12;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            this.gridControl1.Click += new System.EventHandler(this.gridControl1_Click);
+            // 
+            // usersBindingSource
+            // 
+            this.usersBindingSource.DataMember = "Users";
+            this.usersBindingSource.DataSource = this.qlnguoidungDataSet1;
+            // 
+            // qlnguoidungDataSet1
+            // 
+            this.qlnguoidungDataSet1.DataSetName = "qlnguoidungDataSet1";
+            this.qlnguoidungDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colID,
+            this.colUsername,
+            this.colPassword,
+            this.colBirthDate,
+            this.colIsActive});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // colID
+            // 
+            this.colID.FieldName = "ID";
+            this.colID.MinWidth = 25;
+            this.colID.Name = "colID";
+            this.colID.Visible = true;
+            this.colID.VisibleIndex = 0;
+            this.colID.Width = 94;
+            // 
+            // colUsername
+            // 
+            this.colUsername.FieldName = "Username";
+            this.colUsername.MinWidth = 25;
+            this.colUsername.Name = "colUsername";
+            this.colUsername.Visible = true;
+            this.colUsername.VisibleIndex = 1;
+            this.colUsername.Width = 94;
+            // 
+            // colPassword
+            // 
+            this.colPassword.FieldName = "Password";
+            this.colPassword.MinWidth = 25;
+            this.colPassword.Name = "colPassword";
+            this.colPassword.Visible = true;
+            this.colPassword.VisibleIndex = 2;
+            this.colPassword.Width = 94;
+            // 
+            // colBirthDate
+            // 
+            this.colBirthDate.FieldName = "BirthDate";
+            this.colBirthDate.MinWidth = 25;
+            this.colBirthDate.Name = "colBirthDate";
+            this.colBirthDate.Visible = true;
+            this.colBirthDate.VisibleIndex = 3;
+            this.colBirthDate.Width = 94;
+            // 
+            // colIsActive
+            // 
+            this.colIsActive.FieldName = "IsActive";
+            this.colIsActive.MinWidth = 25;
+            this.colIsActive.Name = "colIsActive";
+            this.colIsActive.Visible = true;
+            this.colIsActive.VisibleIndex = 4;
+            this.colIsActive.Width = 94;
+            // 
+            // qlnguoidungDataSetBindingSource
+            // 
+            this.qlnguoidungDataSetBindingSource.DataSource = this.qlnguoidungDataSet;
+            this.qlnguoidungDataSetBindingSource.Position = 0;
+            // 
+            // qlnguoidungDataSet
+            // 
+            this.qlnguoidungDataSet.DataSetName = "qlnguoidungDataSet";
+            this.qlnguoidungDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // gridLookUpEdit1
+            // 
+            this.gridLookUpEdit1.Location = new System.Drawing.Point(322, 380);
+            this.gridLookUpEdit1.Name = "gridLookUpEdit1";
+            this.gridLookUpEdit1.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.gridLookUpEdit1.Properties.PopupView = this.gridLookUpEdit1View;
+            this.gridLookUpEdit1.Size = new System.Drawing.Size(8, 22);
+            this.gridLookUpEdit1.TabIndex = 13;
+            // 
+            // gridLookUpEdit1View
+            // 
+            this.gridLookUpEdit1View.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.RowFocus;
+            this.gridLookUpEdit1View.Name = "gridLookUpEdit1View";
+            this.gridLookUpEdit1View.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridLookUpEdit1View.OptionsView.ShowGroupPanel = false;
+            // 
+            // usersTableAdapter
+            // 
+            this.usersTableAdapter.ClearBeforeFill = true;
+            // 
+            // txtdate
+            // 
+            this.txtdate.Location = new System.Drawing.Point(211, 130);
+            this.txtdate.Name = "txtdate";
+            this.txtdate.Size = new System.Drawing.Size(364, 22);
+            this.txtdate.TabIndex = 14;
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(52, 122);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(56, 16);
+            this.labelControl5.TabIndex = 15;
+            this.labelControl5.Text = "Brith Date";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 450);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.txtdate);
+            this.Controls.Add(this.gridLookUpEdit1);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.txtis);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnthem);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.usersBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qlnguoidungDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qlnguoidungDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qlnguoidungDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLookUpEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridLookUpEdit1View)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnxoa;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtis;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.BindingSource qlnguoidungDataSetBindingSource;
+        private qlnguoidungDataSet qlnguoidungDataSet;
+        private DevExpress.XtraEditors.GridLookUpEdit gridLookUpEdit1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridLookUpEdit1View;
+        private qlnguoidungDataSet1 qlnguoidungDataSet1;
+        private System.Windows.Forms.BindingSource usersBindingSource;
+        private qlnguoidungDataSet1TableAdapters.UsersTableAdapter usersTableAdapter;
+        private DevExpress.XtraGrid.Columns.GridColumn colID;
+        private DevExpress.XtraGrid.Columns.GridColumn colUsername;
+        private DevExpress.XtraGrid.Columns.GridColumn colPassword;
+        private DevExpress.XtraGrid.Columns.GridColumn colBirthDate;
+        private DevExpress.XtraGrid.Columns.GridColumn colIsActive;
+        private System.Windows.Forms.TextBox txtdate;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+    }
+}
+
---
+++ 2025-07-09/Dx-sql/Dx-sql/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Dx_sql
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=qlnguoidung;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+        private string str;
+
+        public Form1(string connectionstring)
+        {
+            this.str = connectionstring;
+            }
+
+        void LoadData()
+        {
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+                command = new SqlCommand("SELECT * FROM Users", connection);
+                adapter.SelectCommand = command;
+                table.Clear();
+                adapter.Fill(table);    
+                gridControl1.DataSource = table;
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối SQL: " + ex.Message);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+                LoadData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối SQL: " + ex.Message);
+            }
+
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = "DELETE FROM Users WHERE ID = @id";
+            command.Parameters.AddWithValue("@id", int.Parse(txtid.Text));
+            LoadData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            command.CommandText = $"UPDATE Users SET Username = '{txtname.Text}' , password = '{txtpass.Text}', birthdate = '{DateTime.Parse(txtdate.Text)}', ISactive ='{bool.Parse(txtis.Text)}' where id = '{txtid.Text}'";
+            
+            LoadData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            command = connection.CreateCommand();
+            DateTime txtdate2 = DateTime.Parse(txtdate.Text);
+            bool txtis2 = bool.Parse(txtis.Text);
+            command.CommandText = $"INSERT INTO Users (USERNAME,PASSWORD, BIRTHDATE,ISACTIVE) VALUES ('{txtname.Text}' , '{txtpass.Text}','{txtdate2}','{txtis2}')";
+            
+            LoadData();
+        }
+
+        private void gridControl1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                if (gridControl1.DataSource is DataTable dt)
+                {
+                    var gridView = gridControl1.MainView as DevExpress.XtraGrid.Views.Grid.GridView;
+                    if (gridView != null)
+                    {
+                        int rowHandle = gridView.FocusedRowHandle;
+                        if (rowHandle >= 0 && rowHandle < dt.Rows.Count)   
+                        {
+                            DataRow row = dt.Rows[rowHandle];
+                            txtid.Text = row["Id"].ToString();
+                            txtname.Text = row["Username"].ToString();
+                            txtpass.Text = row["Password"].ToString();
+                            txtdate.Text = row["Birthdate"].ToString();
+                            txtis.Text = row["IsActive"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void labelControl2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadDataToGrid()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand("SELECT * FROM Users", connection))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(command);
+                        DataTable table = new DataTable();
+                        adapter.Fill(table);
+                        gridControl1.DataSource = table;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void txtid_TextChanged(object sender, EventArgs e)
+        {
+
+            if (int.TryParse(txtid.Text, out int userId))
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        using (SqlCommand command = new SqlCommand("SELECT * FROM Users WHERE Id = @id", connection))
+                        {
+                            command.Parameters.AddWithValue("@id", userId);
+                            using (SqlDataReader reader = command.ExecuteReader())
+                            {
+                                if (reader.Read())
+                                {
+                                    txtname.Text = reader["Username"].ToString();
+                                    txtpass.Text = reader["Password"].ToString();
+                                    txtdate.Text = reader["Birthdate"].ToString();
+                                    txtis.Text = reader["IsActive"].ToString();
+                                }
+                                else
+                                {
+                                    txtname.Text = "";
+                                    txtpass.Text = "";
+                                    txtdate.Text = "";
+                                    txtis.Text = "";
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                txtname.Text = "";
+                txtpass.Text = "";
+                txtdate.Text = "";
+                txtis.Text = "";
+            }
+        }
+    }
+}
+
